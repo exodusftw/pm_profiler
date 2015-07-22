854361b84bf181b51184c8e847047801adfe7a54
Jeremy Grant <exodusftw@users.noreply.github.com>
2015-07-22 13:13:02 -0500

    add dir for /etc/pm-profiler

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,4 +6,13 @@ class pm_profiler::install (
     ensure => $pm_profiler::pm_profiler_version,
     notify => Service['pm-profiler'],
   }
+  
+  file { '/etc/pm-profiler':
+    ensure  => directory,
+    mode    => '0755',
+    owner   => 'root',
+    group   => 'root',
+    require => Package['pm-profiler'],
+  }
+
 }
