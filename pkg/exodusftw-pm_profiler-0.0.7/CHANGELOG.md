ec32fb39835b3a01e55b495b13395e0a50e06ca4
Jeremy Grant <jeremy.grant@outlook.com>
2015-07-22 21:35:02 -0500

    removed pkg dir

--- pkg/exodusftw-pm_profiler-0.0.7/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# Changelog
-
-
-### 2015-07-19 Jeremy Grant 
-* initial Creation of Class to manage SUSE power management profiles
-
-
--- pkg/exodusftw-pm_profiler-0.0.7/manifests/params.pp
+++
@@ -1,6 +0,0 @@
-#Default parameters for the SUSE power management profiles
-class pm_profiler::params {
-
-  $pm_profiler_version = 'present'
-
-}
